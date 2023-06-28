9947e7b4db7a286d1f584db37b451c43805dbd95
Thanawat Jaimoi <tong3089@gmail.com>
2023-06-28 23:03:11 +0700

    create getHandScore function

---
+++ getHandScore.js
@@ -0,0 +1,57 @@
+const SPECIAL_RANK_SCORE = {
+  A: 11,
+  J: 10,
+  Q: 10,
+  K: 10,
+};
+
+function generateCardsInfo(cards = []) {
+  const cardsInfo = cards.map((card) => {
+    const suit = card.substring(0, 1);
+    const rank = card.substring(1);
+    return {
+      suit,
+      rank: !isNaN(rank) ? parseInt(rank) : rank,
+    };
+  });
+  return cardsInfo;
+}
+
+function getHandScore(input = "") {
+  const cards = input.toUpperCase().split(" ");
+  const cardsInfo = generateCardsInfo(cards);
+
+  const cardSuitScores = {
+    H: 0,
+    C: 0,
+    D: 0,
+    S: 0,
+  };
+
+  const firstCard = cardsInfo[0];
+  const isSameRank = cardsInfo.every((card) => card.rank === firstCard.rank);
+  if (isSameRank) {
+    if (firstCard.rank === "A") {
+      return 35;
+    } else {
+      return 32.5;
+    }
+  }
+
+  for (const card of cardsInfo) {
+    const score = SPECIAL_RANK_SCORE[card.rank] || card.rank;
+    cardSuitScores[card.suit] += score;
+  }
+
+  // You can also use Math.max(...Object.values(cardSuitScores))
+  let maxScore = 0;
+  for (const suit in cardSuitScores) {
+    if (cardSuitScores[suit] > maxScore) {
+      maxScore = cardSuitScores[suit];
+    }
+  }
+
+  return maxScore;
+}
+
+console.log(getHandScore("S8 S10 CA"));
